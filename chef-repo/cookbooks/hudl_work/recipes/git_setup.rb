bd7bc807353dca75c9b9f32534b7b511fa52961d
Christopher Lewis <chris.lewis@hudl.com>
2016-01-20 12:38:25 +0000

    ssh working, just need to run bash on vagrant ssh

--- chef-repo/cookbooks/hudl_work/recipes/git_setup.rb
+++ chef-repo/cookbooks/hudl_work/recipes/git_setup.rb
@@ -12,5 +12,57 @@ template '/home/chris/.gitconfig' do
     })
 end
 
+## if a github ssh key exists then copy this over to the ssh folder
+file '/home/chris/.ssh/github_rsa' do
+    owner 'chris'
+    mode '0600'
+    only_if { File.exists?('synced_ssh/github_rsa') }
+end
+
+execute 'copy-github-publickey' do
+    command 'cat /synced_ssh/github_rsa >> /home/chris/.ssh/github_rsa'
+    only_if { File.exists?('synced_ssh/github_rsa') }
+end
+
+# add a bashrc to start up the ssh agent and add github key
+file '/home/chris/.bashrc' do
+    owner 'chris'
+    group 'chris'
+    mode '0700'
+    action :create
+    content 'env=/home/chris/.ssh/agent.env
+            if [ ! -f "$env" ]; then
+                touch $env
+            fi
+            agent_is_running() {
+                if [ "$SSH_AUTH_SOCK" ]; then
+                    ssh-add -l >/dev/null 2>&1 || [ $? -eq 1 ]
+                else
+                    false
+                fi
+            }
+            agent_has_keys() {
+                ssh-add -l >/dev/null 2>&1
+            }
+            agent_load_env() {
+                . "$env" >/dev/null
+            }
+            agent_start() {
+                (umask 077; ssh-agent >"$env")
+                . "$env" >/dev/null
+            }
+            if ! agent_is_running; then
+                agent_load_env
+            fi
+            if ! agent_is_running; then
+                agent_start
+                ssh-add /home/chris/.ssh/github_rsa
+            elif ! agent_has_keys; then
+                ssh-add /home/chris/.ssh/github_rsa
+            fi
+            unset env
+            cd ~/.ssh'
+end
+
 
 
--- chef-repo/cookbooks/hudl_work/recipes/user_create.rb
+++ chef-repo/cookbooks/hudl_work/recipes/user_create.rb
@@ -1,5 +1,9 @@
+group 'chris' do
+    action :create
+end 
+
 user 'chris' do
-  group 'root'
+  group 'chris'
   home '/home/chris'
   shell '/bin/bash'
 end
@@ -7,12 +11,29 @@ end
 # The home directory won't be created unless there's a flag set in etc/login.defs
 directory '/home/chris' do
   owner 'chris'
-  group 'root'
+  group 'chris'
   mode '0755'
   action :create
 end
 
 # create a ssh directory
 directory '/home/chris/.ssh' do
+  owner 'chris'
+  group 'chris'
+  mode '0755'
   action :create
 end
+
+# add my public key to authorized_keys
+file '/home/chris/.ssh/authorized_keys' do
+    action :create
+end
+
+# if a id_rsa key exists in /synced_ssh then stick that into the authorized_keys
+execute 'add-authorized-key' do
+    command 'cat /synced_ssh/id_rsa.pub >> /home/chris/.ssh/authorized_keys'
+    only_if { File.exists?('synced_ssh/id_rsa.pub') }
+end
+
+
+
